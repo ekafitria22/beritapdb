1c4bd4fca9e5bd8c059d17d7d97239954bb0628f
ekafitria22 <ekafitriak00@gmail.com>
2026-02-18 16:05:07 +0800

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,1 +1,42 @@
+# utils/constants.py
+
+APP_TITLE = "Dashboard Klasifikasi Berita Ekonomi Pergerakan PDB Indonesia"
+
+CATEGORY_SITEID = {
+    "detikNews": 3,
+    "detikFinance": 29,
+    "detikSport": 21,
+    "Sepakbola": 27,
+    "detikHealth": 55,
+    "detikHot": 32,
+    "Wolipop": 63,
+    "detikInet": 5,
+    "detikFood": 35,
+    "detikOto": 44,
+    "detikTravel": 66,
+    "detikEdu": 117,
+    "detikHikmah": 153,
+    "detikJateng": 118,
+    "detikJatim": 119,
+    "detikJabar": 120,
+    "detikSulsel": 121,
+    "detikSumut": 122,
+    "detikBali": 123,
+}
+
+NEWS_TYPE_OPTIONS = [
+    "Berita Ekonomi",
+    "Berita Politik",
+    "Berita Teknologi",
+    "Berita Olahraga",
+    "Berita Hiburan",
+    "Semua",
+]
+
+# default keyword hint (kamu bisa ubah/luaskan)
+KEYWORD_HINT = [
+    "PDB", "GDP", "pertumbuhan ekonomi", "inflasi", "investasi",
+    "kebijakan moneter", "ekspor", "impor", "konsumsi", "produksi",
+    "kuartal", "YoY", "QoQ", "kumulatif",
+]
 
